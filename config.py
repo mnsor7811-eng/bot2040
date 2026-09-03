1023baa4f2648e4b34c1f7e4d1b2b56588167dae
mnsor7811-eng <mnsor7811@gmail.com>
2026-09-04 02:40:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -74,7 +74,7 @@ def http_post_form(url, data_dict, headers=None, timeout=12):
         return None
 
 # ==================== الإعدادات الأساسية ====================
-TOKEN = os.getenv('TOKEN', '8927305428:AAH7CGvaZRpXE7whw5dIr8B9UF-tePExmAk')
+TOKEN = os.getenv('TOKEN', '8927305428:AAHwGoZmkqZ0Rsv5NrX2x9CH-5JcrO4a-uA')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', 'AQ.Ab8RN6IOLYCW3mnMh6H5le6Bc1pAG60TXO0IoxjpPcHvaFZHkg')
 ADMIN_ID = int(os.getenv('ADMIN_ID', 6113734300))
 ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', '@Num_s7')
