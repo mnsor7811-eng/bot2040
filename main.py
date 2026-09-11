b5dfe26cfcedeaac635e06baadabe7e5ac432e81
mnsor7811-eng <mnsor7811@gmail.com>
2026-09-11 22:31:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -892,10 +892,10 @@ def get_main_welcome_text(user_id, name="", username="", balance=0.0):
     sp2 = " " * max(3, target_width - ch2_len - u2_len)
     sp3 = " " * max(3, target_width - ch3_len - u3_len)
 
-    line1 = f"{rlm}<a href=\"{ch_off}\">📎قناة البوت الرسمية</a>{sp1}{ltr}💙 {user_disp} 💙{ltr}"
-    line2 = f"{rlm}<a href=\"{ch_ord}\">🛒قناة التفعيلات</a>{sp2}{ltr}👤 : <code>{user_id}</code> •{ltr}"
-    line3 = f"{rlm}<a href=\"{ch_tut}\">📚قناة شرح الاستخدام</a>{sp3}{ltr}💲 : {bal_str}$ •{ltr}"
-    line4 = f"{rlm}<a href=\"{ch_exp}\">🎬قناة الشروحات </a>"
+    line1 = f"{rlm}<a href=\"{ch_off}\">📎قناة البوت الرسمية</a>   {sp1}{ltr}💙 {user_disp} 💙{ltr}"
+    line2 = f"{rlm}<a href=\"{ch_ord}\">🛒قناة التفعيلات</a>  {sp2}{ltr}👤 : <code>{user_id}</code> •{ltr}"
+    line3 = f"{rlm}<a href=\"{ch_tut}\">📚قناة التعليمات</a>  {sp3}{ltr}💲 : {bal_str}$ •{ltr}"
+    line4 = f"{rlm}<a href=\"{ch_exp}\">🎬قناة الشروحات</a>"
 
     return (
         "• أهلاً بك في بوت 🎁 SMS SMM STORE\n\n"
